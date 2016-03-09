755b83b456d4dd455855e7756cfb2dfab76178e5
Tim Waters <chippy2005@gmail.com>
2016-03-09 19:31:25 +0000

    refactoring it a bit more messy but still...

--- templatehell.rb
+++ templatehell.rb
@@ -14,12 +14,12 @@ div class='asccas'>
 
 {{Map
 |param1 = value1
-|param2=value2
+|latitude=345
 |param3
 |param4=
 |nested1 = {{ru|Тарту}}
 |nested2 = {{ru|1=СССР}}
-}} 
+}}
 
 
 {{Another
@@ -32,8 +32,73 @@ div class='asccas'>
 }}"
 
 
+def change_template(str, bbox)
+  map_attrs = str.split("|")
+  new_attrs = []
+  map_template_attrs = []
+  
+  #bbox_array  = bbox.split(",").map{|s| s.to_f.round(7)}
+#  longitude = "#{bbox_array[0]}/#{bbox_array[2]}"
+  #latitude = "#{bbox_array[1]}/#{bbox_array[3]}"
+  longitude = "123"
+  latitude = "345"
+  
+  something_changed  = nil
+          
+  map_attrs.each do | map_attr |
 
-def end_template(str)
+    if map_attr.split("=").size == 2 && !map_attr.include?("<!--")
+
+      if map_attr.include? "warped"
+        unless map_attr.split("=")[1].include?("yes")  # don't edit if it's already there
+          something_changed = true
+          map_attr = "warped=yes\n"
+        end    
+      end
+
+      if map_attr.include? "latitude"
+        if map_attr.split("=")[1].strip != latitude
+          something_changed = true
+          map_attr = "latitude=#{latitude}\n"  
+        end
+      end
+      
+      if map_attr.include? "longitude"
+        if map_attr.split("=")[1].strip != longitude
+          something_changed = true
+          map_attr = "longitude=#{longitude}\n"  
+        end
+      end
+      
+      map_template_attrs << map_attr
+      
+    end ## size > 2
+    
+    new_attrs << map_attr
+    
+  end
+  
+  #if it has help warp but no warped, we have to add in warped
+  if map_template_attrs.any? { | s| (s.include?("help warp") or s.include?("help_warp"))} && map_template_attrs.none? {|s| s.include? "warped"}
+    something_changed = true
+    new_attrs.insert(2, "warped=yes\n")
+  end
+  
+  map_template = new_attrs.join("|")
+
+  if something_changed
+    return map_template
+  else
+    return nil
+  end
+
+end
+
+#
+# returns the index of the end of the first template in the passed in string 
+# will return false if no ending characters can be determined
+#
+def end_index_template(str)
   
   open_bracket = ["{", "{"]
   close_bracket = ["}", "}"]
@@ -64,18 +129,26 @@ template_end_index  = nil
 puts "start in string = " + string_start_index.to_s
 if map_match
   map_template = map_match[0]
-  template_end_index = end_template(map_template)
+  template_end_index = end_index_template(map_template)
 end
 
-puts "end index in template = "+template_end_index.to_s
+if map_template && template_end_index
+  puts "end index in template = "+template_end_index.to_s
 
-map_template_string =  string[string_start_index..(string_start_index+template_end_index)]
+  map_template_string =  string[string_start_index..(string_start_index+template_end_index)]
 
-puts map_template_string
-
-puts "replacing"
-
-string[string_start_index..(string_start_index+template_end_index+1)] = "foo bar"
-
-puts "new string"
-puts string
+  puts map_template_string
+  
+  bbox = "1232,123123312312,123132"
+  new_template = change_template(map_template_string, bbox)
+  
+  if new_template 
+    string[string_start_index..(string_start_index+template_end_index+1)] = new_template
+    puts "changing"
+  else
+    puts "nothing changed"
+  end
+  
+  puts "string"
+  puts string
+end
